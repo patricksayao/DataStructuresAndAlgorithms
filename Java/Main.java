15166e67ffa254472fd2eb97f56c2346354dbf91
patricksayao <77041730+patricksayao@users.noreply.github.com>
2022-03-28 16:23:48 +0800

    Create Main.java

---
+++ Java/Main.java
@@ -0,0 +1,43 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("hello");
+        // int[] arr1 = {1,2,3,4,5,6,7,8,9,10};
+        // int[] arr2 = {1,2,3,4,5};
+        // int[] arr3 = {6,7,8,9,5};
+        int[] arr4 = {2,7,22};
+        int[] arr5 = {3,9,19,21};
+
+        //int[] arrItems = new int[20];
+        // for (int i : arrItems){
+        //     System.out.println(i);
+        // }
+        
+        //String[] arrStrings = {"A", "B", "C", "D", "E"};
+
+        //String str = "Hello";
+
+        //FirstClass.PrintItemsInArray(arrItems);
+
+        //FirstClass.ArrayPairs(arr2);
+
+        //FirstClass.ArrayPairs2(arrStrings);
+
+        //FirstClass.SumOfElements(arr2);
+
+        // boolean hasPair = SecondClass.CommonArrayItem(arr2, arr3);
+        // System.out.println(hasPair);
+
+        // boolean hasPair = SecondClass.CommonArrayItem2(arr2, arr3);
+        // System.out.println(hasPair);
+
+        //ThirdClass.CreatingStack();
+
+        // string reverse exercises-------------
+        //System.out.println(ThirdClass.StringReverse2(str));
+
+        // merge sorted arrays exercises------------
+        MergeSortArrays.MergeAndSortArrays(arr4, arr5);
+    }
+}
